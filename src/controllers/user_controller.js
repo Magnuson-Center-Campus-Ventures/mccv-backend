b621fbaf0463eca93a840f3d34b4b5f9b397a532
Sarah Hong <shong88tx@gmail.com>
2020-05-30 10:07:02 -0500

    signup works yay

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import jwt from 'jwt-simple';
 import User from '../models/user_model';
-// import Student from '../models/student_model';
+import Student from '../models/student_model';
 
 dotenv.config({ silent: true });
 
@@ -29,21 +29,31 @@ export const signup = (req, res) => {
         user.role = req.body.role;
         user.student_profile_id = req.body.student_profile_id;
         user.startup_id = req.body.startup_id;
-        user.save().then((result) => {
-          /*
-          if (user.role === 'student') {
-            Student.createStudent({ user_id: user._id });
-          } else if (user.role === 'startup') {
-            // create startup
-          }
-          */
-          res.send({ token: tokenForUser(result), id: user._id });
-        }).catch((error) => {
-          res.status(500).json({ error });
-        });
+        if (user.role === 'student') {
+          const student = new Student();
+          user.save().then((result) => {
+            res.send({ token: tokenForUser(result), id: user._id });
+          }).then((result2) => { // if saved user, save a student with user_id prefilled
+            student.user_id = user._id;
+            student.save().then((result3) => {
+              // if saved student, update previously created user with student_profile_od
+              user.student_profile_id = student._id;
+              User.findByIdAndUpdate(user._id, user, { new: true }).then((result4) => {
+                console.log(result4);
+              }).catch((error) => {
+                res.status(500).json({ error });
+              });
+            }).catch((error) => {
+              res.status(500).json({ error });
+            });
+          }).catch((error) => {
+            res.status(500).json({ error });
+          });
+        } else if (user.role === 'startup') {
+          console.log('startup');
+        }
       }
-    })
-    .catch((error) => {
+    }).catch((error) => {
       res.status(500).json({ error });
     });
 };
