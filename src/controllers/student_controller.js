2e479e5bd823e1110c57f7e5c6f99eb74dec545e
Rehoboth Okorie <okorie.23@dartmouth.edu>
2021-08-22 13:32:05 -0500

    fixed job_search_status bug

--- src/controllers/student_controller.js
+++ src/controllers/student_controller.js
@@ -19,6 +19,7 @@ export const createStudent = ((req, res) => {
   student.relevant_classes = req.body.relevant_classes;
   student.other_exp = req.body.other_exp;
   student.status = req.body.status;
+  student.job_search_status = req.body.job_search_status
   student.desired_start_date = req.body.desired_start_date;
   student.desired_end_date = req.body.desired_end_date;
   student.time_commitment = req.body.time_commitment;
